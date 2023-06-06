0321f0aa13ec55c0fb1f827a5ef114472c90c643
Niih <rodriguesniih2005@gmail.com>
2023-06-05 23:15:00 -0300

    Special Characters Work

--- Caesar/caesarhard.c
+++ Caesar/caesarhard.c
@@ -2,9 +2,13 @@
 #include <ctype.h>
 
 int main(int argc, char* argv[]){
-   int key = 1;
+   int key;
+   
+    printf("Type the Key: ");
+    scanf("%d", &key);
+
    char* name = "Hello, Zeta!";
-  
+
    for(int i = 0; name[i] != '0'; i++) {
       if(isalpha(name[i])) {
       if(isupper(name[i])) {
