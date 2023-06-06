af287c77ce08855d6cbffeead85879740c5fdcc0
Niih <rodriguesniih2005@gmail.com>
2023-06-05 23:51:10 -0300

    Special Characters Work

---
+++ Caesar/caesar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(int argc, char* argv[]){
+   int key = 1;
+   char* name = "Hello, Zeta!";
+
+   for(int i = 0; name[i] != '0'; i++) {
+      if(isalpha(name[i])) {
+      if(isupper(name[i])) {
+          printf("%c", (name[i] - 'A' + key) % 26 + 'A');
+      }
+      if(islower(name[i])) {
+         printf("%c", (name[i] - 'a' + key) % 26 + 'a');
+      }
+      }
+      if(ispunct(name[i])) {
+         printf("%c", name[i]);
+      }
+      if(isspace(name[i])) {
+         printf("%c", name[i]);
+      }
+   }
+   printf("\n");
+} 
---
+++ Caesar/caesarhard.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(int argc, char* argv[]){
+   int key;
+   
+    printf("Type the Key: ");
+    scanf("%d", &key);
+
+   char* name = "Hello, Zeta!";
+
+   for(int i = 0; name[i] != '0'; i++) {
+      if(isalpha(name[i])) {
+      if(isupper(name[i])) {
+          printf("%c", (name[i] - 'A' + key) % 26 + 'A');
+      }
+      if(islower(name[i])) {
+         printf("%c", (name[i] - 'a' + key) % 26 + 'a');
+      }
+      }
+      if(ispunct(name[i])) {
+         printf("%c", name[i]);
+      }
+      if(isspace(name[i])) {
+         printf("%c", name[i]);
+      }
+   }
+   printf("\n");
+} 
