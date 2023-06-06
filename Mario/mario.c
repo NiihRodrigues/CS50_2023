b8c582cbd2dc10270aadbc1e2057933af7a33874
Niih <rodriguesniih2005@gmail.com>
2023-06-05 21:52:58 -0300

    first commit

---
+++ Mario/mario.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void){
+  int height;
+  int hashcount = 1;
+  int spacecout; 
+  
+  do {
+    printf("Height: ");
+    scanf("%d", &height);
+  } while (height < 1 || height > 8);
+
+  for (int i = 0; i < height; i++) {
+    spacecout = height - hashcount;
+
+    for (int j = 0; j < spacecout; j++) {
+      printf(" ");
+    }
+
+    for (int k = 0; k < hashcount; k++) {
+       printf("#");
+    }
+
+    printf("\n");
+    hashcount++;
+  }
+
+  return 0;
+}
